3840e7bf9fbac7dc34850af24d6ada228a724bf7
amrdawoud <99187142+amrbdawoud@users.noreply.github.com>
2025-03-14 03:54:56 +0200

    error middleware

---
+++ Middlewares/ErrorMiddleware.cs
@@ -0,0 +1,35 @@
+using System.Globalization;
+using socialfeed.Models;
+
+namespace Middleware.Example;
+
+public class ErrorMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            // Call the next delegate/middleware in the pipeline.
+            await _next(context);
+
+        }
+        catch (socialfeed.Errors.BaseException e)
+        {
+            context.Response.StatusCode = e.StatusCode;
+            await context.Response.WriteAsJsonAsync(e.Message);
+
+        }
+        catch (Exception e)
+        {
+            context.Response.StatusCode = 500;
+            await context.Response.WriteAsJsonAsync($"internal server error: {e.Message}");
+        }
+    }
+}
